d5b9a32f201998000981ce356202ae3cde873b42
chupedan <miinhtri1310@gmail.com>
2023-09-29 22:45:10 +0700

    calculator

--- MyEx6/app/src/main/java/com/diemminhtri/myex6/MainActivity.java
+++ MyEx6/app/src/main/java/com/diemminhtri/myex6/MainActivity.java
@@ -3,12 +3,39 @@ package com.diemminhtri.myex6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etNum1, etNum2;
+    private Button btnPlus, btnMinus, btnMul, btnDivide;
+    private TextView tvResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMul = findViewById(R.id.btnMul);
+        btnDivide = findViewById(R.id.btnDivide);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnPlus.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        double num1 = Double.parseDouble(etNum1.getText().toString());
+        double num2 = Double.parseDouble(etNum2.getText().toString());
+        double cal = calAll(num1, num2);
+    }
+    public double calAll(double num1, double num2) {
+        return num1 + num2;
     }
 }
